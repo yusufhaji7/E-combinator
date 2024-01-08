44b5d5b31864b895efdef46620988183855f7e1a
yusufhaji7 <yusufhadji873@gmail.com>
2024-01-08 09:26:37 +0300

    responsiveness

--- src/components/Aboutus.jsx
+++ src/components/Aboutus.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import Fuel from "../assets/Fuel.png";
-
+ // Import the CSS file for styling
 
 function About() {
   return (
     <div className='about-section'>
       <h2 className='about-title'>About Us</h2>
       <div className='about-content'>
-        
-        <div className='about-text'>
-          Our accelerator program is designed to provide early-stage startups with the tools and resources they need to 
-          succeed. From mentorship and networking opportunities to resources and guidance, we offer a comprehensive support
-          system to help startups navigate the challenges of starting and growing a business.
-        </div>
         <img className='about-image' src={Fuel} alt="Fuel" />
+        <div className='about-text'>
+          <p>
+            Our accelerator program is designed to provide early-stage startups with the tools and resources they need to 
+            succeed. From mentorship and networking opportunities to resources and guidance, we offer a comprehensive support
+            system to help startups navigate the challenges of starting and growing a business.
+          </p>
+        </div>
       </div>
     </div>
   );
