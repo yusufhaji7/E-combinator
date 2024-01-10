c9462450fc15e3fcd6ca93783e2e28def1041991
yusufhaji7 <yusufhadji873@gmail.com>
2024-01-10 23:24:45 +0300

    blod

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,4 +1,5 @@
 // Blog.js
+
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Development from '../assets/dev.png';
@@ -15,15 +16,13 @@ const Blog = () => {
     <div className="blog-container flex flex-wrap space-x-4 space-y-8">
       <h2 className="w-full text-3xl font-bold mb-4">Latest Blogs</h2>
       {blogPosts.map((post) => (
-        
-          <Link to={`/blog/${post.id}`} className="block w-full sm:w-1/2 md:w-1/3 lg:w-1/2 xl:w-1/4 p-0 mb-4 hover:shadow-lg transition duration-300 ease-in-out transform hover:scale-105">
-            <img src={post.image} alt={post.title} style={{ width: '100%', borderRadius: '8px 8px 0 0' }} />
-            <div className="p-4 bg-white border border-t-0 border-gray-200 border-solid">
-              <h3 className="text-xl font-semibold mb-2 hover:text-blue-500 transition duration-300 ease-in-out">{post.title}</h3>
-              <p className="text-gray-600">{post.content}</p>
-            </div>
-          </Link>
-        
+        <Link key={post.id} to={`/blog/${post.id}`} className="block w-full sm:w-1/2 md:w-1/3 lg:w-1/2 xl:w-1/4 p-0 mb-4 hover:shadow-lg transition duration-300 ease-in-out transform hover:scale-105">
+          <img src={post.image} alt={post.title} style={{ width: '100%', borderRadius: '8px 8px 0 0' }} />
+          <div className="p-4 bg-white border border-t-0 border-gray-200 border-solid">
+            <h3 className="text-xl font-semibold mb-2 hover:text-blue-500 transition duration-300 ease-in-out">{post.title}</h3>
+            <p className="text-gray-600">{post.content}</p>
+          </div>
+        </Link>
       ))}
     </div>
   );
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,60 +1,54 @@
-import React from 'react'
-import { GiOfficeChair } from "react-icons/gi";
-import { GiTeacher } from "react-icons/gi";
-import { RiRefund2Fill } from "react-icons/ri";
+// Services.js
+
+import React from 'react';
+import { GiOfficeChair, GiTeacher, RiRefund2Fill } from "react-icons/all";
 
 function Services() {
   return (
-    <section className='service-section'>
-      <div className="row">
-        <h2 className="section-heading">Our Services</h2>
-      </div>
-      <div className="row">
-        <div className="column">
-          <div className="card">
-            <div className="icon-wrapper">
-              <i className="fas fa-hammer">
-                <GiOfficeChair/>
-              </i>
+    <>
+      <div className="section-divider" />
+      <section className='service-section'>
+        <div className="row">
+          <h2 className="section-heading">Our Services</h2>
+        </div>
+        <div className="row">
+          <div className="column">
+            <div className="card">
+              <div className="icon-wrapper">
+                <GiOfficeChair />
+              </div>
+              <h3>Working space</h3>
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam consequatur necessitatibus eaque.
+              </p>
             </div>
-            <h3>Working space</h3>
-            <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam
-              consequatur necessitatibus eaque.
-            </p>
+          </div>
+          <div className="column">
+            <div className="card">
+              <div className="icon-wrapper">
+                <GiTeacher />
+              </div>
+              <h3>Mentorship</h3>
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam consequatur necessitatibus eaque.
+              </p>
+            </div>
+          </div>
+          <div className="column">
+            <div className="card">
+              <div className="icon-wrapper">
+                <RiRefund2Fill />
+              </div>
+              <h3>Equity Funding</h3>
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam consequatur necessitatibus eaque.
+              </p>
+            </div>
           </div>
         </div>
-        <div className="column">
-          <div className="card">
-            <div className="icon-wrapper">
-              <i className="fas fa-brush">
-              <GiTeacher />
-              </i>
-            </div>
-            <h3>Mentorship</h3>
-            <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam
-              consequatur necessitatibus eaque.
-            </p>
-          </div>
-        </div>
-        <div className="column">
-          <div className="card">
-            <div className="icon-wrapper">
-              <i className="fas fa-wrench">
-                <RiRefund2Fill/>
-              </i>
-            </div>
-            <h3>Equity Funding</h3>
-            <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam
-              consequatur necessitatibus eaque.
-            </p>
-          </div>
-        </div>
-      </div>
-    </section>
-  )
+      </section>
+    </>
+  );
 }
 
-export default Services
+export default Services;
