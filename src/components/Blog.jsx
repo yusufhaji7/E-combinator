45d50fe6ed7766e1f6d4f682dbeff3591e02ed7e
yusufhaji7 <yusufhadji873@gmail.com>
2024-01-10 23:16:22 +0300

    blod

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,12 +1,13 @@
 // Blog.js
 import React from 'react';
 import { Link } from 'react-router-dom';
- // Import the CSS file for styling
+import Development from '../assets/dev.png';
+import Investor from '../assets/investor.webp';
 
 const Blog = () => {
   const blogPosts = [
-    { id: 1, title: 'First Blog Post', content: 'This is the content of the first blog post.' },
-    { id: 2, title: 'Second Blog Post', content: 'This is the content of the second blog post. ' },
+    { id: 1, title: 'First Blog Post', content: 'This is the content of the first blog post.', image: `${Development}` },
+    { id: 2, title: 'Second Blog Post', content: 'This is the content of the second blog post.', image: `${Investor}` },
     // Add more blog posts as needed
   ];
 
@@ -14,13 +15,15 @@ const Blog = () => {
     <div className="blog-container flex flex-wrap space-x-4 space-y-8">
       <h2 className="w-full text-3xl font-bold mb-4">Latest Blogs</h2>
       {blogPosts.map((post) => (
-        <div key={post.id} className="blog-articles w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/4 p-4 hover:shadow-lg transition duration-300 ease-in-out">
-          {/* Use Link to create clickable links to individual blog posts */}
-          <Link to={`/blog/${post.id}`} className="block">
-            <h3 className="text-xl font-semibold mb-2 hover:text-blue-500 transition duration-300 ease-in-out">{post.title}</h3>
+        
+          <Link to={`/blog/${post.id}`} className="block w-full sm:w-1/2 md:w-1/3 lg:w-1/2 xl:w-1/4 p-0 mb-4 hover:shadow-lg transition duration-300 ease-in-out transform hover:scale-105">
+            <img src={post.image} alt={post.title} style={{ width: '100%', borderRadius: '8px 8px 0 0' }} />
+            <div className="p-4 bg-white border border-t-0 border-gray-200 border-solid">
+              <h3 className="text-xl font-semibold mb-2 hover:text-blue-500 transition duration-300 ease-in-out">{post.title}</h3>
+              <p className="text-gray-600">{post.content}</p>
+            </div>
           </Link>
-          <p className="text-gray-600">{post.content}</p>
-        </div>
+        
       ))}
     </div>
   );
